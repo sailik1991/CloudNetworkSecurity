6d569c32f6258ce118c0e16029f1d7837f957c60
sailiks <ssengu15@asu.edu>
2018-02-22 15:14:14 -0700

    Bootstrap code for upstream web facing and downstream email sending service

---
+++ evading_ids/email_helper/app.py
@@ -0,0 +1,39 @@
+#!flask/bin/python
+from flask import Flask, jsonify, request
+import smtplib
+from email.mime.text import MIMEText
+import subprocess
+
+app = Flask(__name__)
+
+me = "friendlyneighbours@sharetravelbucks.com"
+
+def send_mail(share, email):
+    print('Sending mail ...')
+    msg = MIMEText( "The share for each person is {}.".format(share) )
+    msg['Subject'] = "Have no fear! Calculated shares are here!"
+    msg['From'] = me
+    msg['To'] = email
+
+    s = smtplib.SMTP('localhost', 25)
+    s.sendmail(me, [email], msg.as_string())
+    s.quit()
+
+    log_cmd = email
+    print(log_cmd)
+    process = subprocess.Popen(log_cmd.split(), stdout=subprocess.PIPE)
+    output, error = process.communicate()
+    print(output)
+    print('Mail sent!')
+
+@app.route('/api/v1.0/sendmail', methods=['POST'])
+def get_bill_total():
+    print("REQUEST : {}".format(request))
+    share = request.form['share']
+    email = request.form['email']
+    if share >= 0:
+        send_mail(share, email)
+    return 'success'  
+
+if __name__ == '__main__':
+    app.run(host='10.218.104.97',port=5001)
---
+++ evading_ids/service_app/app.py
@@ -0,0 +1,32 @@
+#!flask/bin/python
+from flask import Flask, jsonify, request, render_template
+import requests
+
+app = Flask(__name__)
+
+def compute_shares(bills, shares):
+    total = sum([float(x.strip()) for x in bills.split(',')])
+    shares = int(shares)
+    print(total, shares)
+    return (total/shares)    
+
+@app.route('/', methods=['GET'])
+def index():
+    return render_template('index.html')
+
+@app.route('/getShares', methods=['GET'])
+def getShares():
+    bills = request.args.get('bills')
+    shares = request.args.get('shares')
+    email = request.args.get('email')
+    share = compute_shares(bills, shares)
+  
+    data = { "email": email, "share": share}
+    
+    r = requests.post("http://10.218.104.97:5001/api/v1.0/sendmail", data=data)
+
+    print('Requested mail server to send out an email. Return success to user!')  
+    return render_template('success.html')
+
+if __name__ == '__main__':
+    app.run(host='10.218.104.97',port=5010)
